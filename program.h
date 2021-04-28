ccc0b4afdce691889ca4392af4eb45f2db0e5fd5
PolinaPo <alexsslave@mail.ru>
2021-04-28 21:16:12 +0700

    Editing Comments

--- program.h
+++ program.h
@@ -6,7 +6,7 @@ using namespace std;
 
 namespace type_plants {
 
-	// общий класс фраз
+	// общий класс растений
 	class plants {
 	public:
 		string name;
@@ -68,5 +68,4 @@ namespace type_plants {
 		~flower();
 	};
 } // end type_plants namespace
-
 #endif // !PROGRAM_H
